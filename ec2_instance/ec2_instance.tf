b44245855924a805d0a29b314d780baecf6b5d15
jsmickey <jsmickey@gmail.com>
2017-03-28 09:45:44 -0500

    Add account name to node name for Chef (#26)

--- ec2_instance/ec2_instance.tf
+++ ec2_instance/ec2_instance.tf
@@ -120,7 +120,7 @@ aws --region ${var.aws_region} s3 cp s3://${var.account_name}-infra/ /etc/chef/t
 chmod 600 /etc/chef/trusted_certs/*
 echo "chef_server_url '${var.chef_server_url}/organizations/default'" >> /etc/chef/client.rb
 echo "trusted_certs_dir '/etc/chef/trusted_certs'" >> /etc/chef/client.rb
-echo "node_name '${var.instance_name}'" >> /etc/chef/client.rb
+echo "node_name '${var.account_name}_${var.instance_name}'" >> /etc/chef/client.rb
 echo "log_location   STDOUT" >> /etc/chef/client.rb
 echo "log_level :info" >> /etc/chef/client.rb
 echo "log_location '/var/log/chef/client.log'" >> /etc/chef/client.rb
