e775fc7bbbe55b10ad38f195251c74edc75b480e
jsmickey <jsmickey@gmail.com>
2017-03-30 09:30:50 -0500

    Vpc fix (#37)
    * Fix var type
    
    * Add ssm association
    
    * Typo

--- ssm_association/ssm_association.tf
+++ ssm_association/ssm_association.tf
@@ -1,7 +1,7 @@
 variable "instance_id" {}
 variable "ssm_document_name" {}
 
-resource "aws_ssm_assoication" "association" {
+resource "aws_ssm_association" "association" {
   name        = "${var.ssm_document_name}"
   instance_id = "${var.instance_id}"
 }
