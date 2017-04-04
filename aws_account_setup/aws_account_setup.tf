4a4cc105969e84f57e462f2e5167f34e0bd4f5ee
coobachey <coobey@gmail.com>
2017-04-04 08:47:14 -0500

    Baseinfra policy (#45)
    * Base Infra Policy
    
    * Adding second policy name to separate

--- aws_account_setup/aws_account_setup.tf
+++ aws_account_setup/aws_account_setup.tf
@@ -1,6 +1,7 @@
 data "aws_iam_account_alias" "current" {}
 
 variable "account_name" {}
+variable "account_name_infra_bucket" {}
 
 variable "billing_admins" {
   type = "list"
@@ -151,4 +152,106 @@ module "backup_bucket" {
   bucket_name    = "${var.account_name}-backups"
   logging_prefix = "${var.account_name}-backups/"
   source         = "github.com/Trility/tf-aws-modules//s3_bucket"
+}
+
+module "iam_policy_base" {
+  policy_name        = "base"
+  policy_description = "base"
+  source             = "github.com/Trility/tf-aws-modules//iam_policy"
+
+  policy = <<POLICY
+{
+    "Statement": [
+        {
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::${var.account_name}-*"
+            ]
+        },
+        {
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:ListBucket"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::${var.account_name_infra_bucket}-infra/*"
+            ]
+        },
+        {
+            "Action": [
+                "s3:PutObject"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::${var.account_name}-backups/*"
+            ]
+        },
+        {
+            "Action": [
+                "s3:Put*",
+                "s3:Get*",
+                "s3:List*"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::${var.account_name}-logs-infra*"
+            ]
+        },
+        {
+            "Action": [
+                "autoscaling:Describe*",
+                "cloudwatch:PutMetricData*",
+                "ec2:CreateTags",
+                "ec2:Describe*",
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply",
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "opsworks-cm:AssociateNode",
+                "opsworks-cm:DescribeNodeAssociationStatus",
+                "opsworks-cm:DescribeServers",
+                "ssm:DescribeAssociation",
+                "ssm:GetDocument",
+                "ssm:ListAssociations",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "*"
+            ]
+        }
+    ],
+    "Version": "2012-10-17"
+}
+POLICY
+}
+
+module "iam_role_base" {
+  role_name = "base"
+  source    = "github.com/Trility/tf-aws-modules//iam_role"
+}
+
+module "iam_role_base_attach_policy" {
+  role_name  = "${module.iam_role_base.role_name}"
+  policy_arn = "${module.iam_policy_base.policy_arn}"
+  source     = "github.com/Trility/tf-aws-modules//iam_role_policy_attachment"
+}
+
+module "iam_instance_profile_base" {
+  profile_name = "base"
+  roles        = ["${module.iam_role_base.role_name}"]
+  source       = "github.com/Trility/tf-aws-modules//iam_instance_profile"
 }
