f4999d16fedb8c82dbf96bbc4a196d0eaeab50a3
John Mickey <john@trility.io>
2017-04-25 10:29:26 -0500

    Proper data format

--- opsworks_chef_automate/opsworks_chef_automate.tf
+++ opsworks_chef_automate/opsworks_chef_automate.tf
@@ -3,7 +3,7 @@ data "aws_region" "current" {
 }
 
 data "aws_ip_ranges" "ec2" {
-  regions  = ["${data.aws_region.current}"]
+  regions  = ["${data.aws_region.current.name}"]
   services = ["amazon", "ec2"]
 }
 
