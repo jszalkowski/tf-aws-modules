e194f5f63368165a5d31a7c70cb9f1f7ff18f2ea
Eric Gerling <ericgerling@gmail.com>
2017-05-09 10:27:26 -0500

    Add hosted_zone_id as an output from the cloudfront distribution (#70)

--- cloudfront_distribution/cloudfront_distribution.tf
+++ cloudfront_distribution/cloudfront_distribution.tf
@@ -32,6 +32,10 @@ output "cloudfront_domain_name" {
   value = "${aws_cloudfront_distribution.distribution.domain_name}"
 }
 
+output "cloudfront_hosted_zone_id" {
+  value = "${aws_cloudfront_distribution.distribution.hosted_zone_id}"
+}
+
 resource "aws_cloudfront_distribution" "distribution" {
   aliases = ["${var.aliases}"]
 
