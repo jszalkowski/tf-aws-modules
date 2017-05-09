91dc2dc1c03974cc41b4dea72403151c2a92fbfb
Eric Gerling <ericgerling@gmail.com>
2017-05-08 20:56:49 -0500

    Add module to redirect using s3 static bucket

---
+++ s3_bucket_static_website_redirect/s3_bucket_static_website_redirect.tf
@@ -0,0 +1,23 @@
+variable "redirect_address" {}
+
+variable "bucket_name" {}
+
+output "bucket_id" {
+  value = "${aws_s3_bucket.bucket.id}"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.bucket_name}"
+  acl = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    redirect_all_requests_to = "${var.redirect_address}"
+  }
+
+  tags {
+    Name = "${var.bucket_name}"
+  }
+}
